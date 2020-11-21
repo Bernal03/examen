846d1dcb705227b05d8e263108181527d6669ea0
Bernal03 <61256617+Bernal03@users.noreply.github.com>
2020-11-21 12:28:13 -0800

    commit1

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import logo from '../logo.webp';
+import '../App.css';
+
+const Header = () => {
+  return (  
+    <header className="App-header">
+      <img src={logo} />
+    </header>
+  );
+}
+
+export default Header;
